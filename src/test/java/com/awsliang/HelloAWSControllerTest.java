2c52a9e70e4abbcddf56adf71a124cd48f5df1b8
Ubuntu <ubuntu@ip-172-31-8-214.us-east-2.compute.internal>
2019-10-26 10:01:34 +0000

    message

---
+++ src/test/java/com/awsliang/HelloAWSControllerTest.java
@@ -0,0 +1,19 @@
+package com.awsliang;
+
+import junit.framework.TestCase;
+
+
+public class HelloAWSControllerTest extends TestCase {
+    
+
+    public void setup() {
+        
+    }
+    
+    public HelloAWSControllerTest(String name) {
+            super( name );
+        }
+    public void testHelloAWS() throws Exception {
+            assertTrue(2>1);
+    }
+}
