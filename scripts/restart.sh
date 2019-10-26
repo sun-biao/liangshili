bced948f9159399f819452659ac2d6f867d958a8
Ubuntu <ubuntu@ip-172-31-8-214.us-east-2.compute.internal>
2019-10-26 18:04:46 +0000

    all

---
+++ scripts/restart.sh
@@ -0,0 +1,4 @@
+su ec2-user
+/opt/apache-tomcat-8.5.47/bin/shutdown.sh
+/opt/apache-tomcat-8.5.47/bin/startup.sh
+echo 'restart finished'
--- scripts/setuppri.sh
+++ scripts/setuppri.sh
@@ -1,3 +1,2 @@
 sudo chown -R ec2-user /opt/apache-tomcat-8.5.47
-/opt/apache-tomcat-8.5.47/bin/shutdown.sh
-/opt/apache-tomcat-8.5.47/bin/startup.sh
+echo 'finished setup pri'
